df45ea515abda92b0c57cf16312991d1da7e66a5
LoL-Bee <abisaipaskalis11@gmail.com>
2025-03-21 10:43:56 +0700

    Initial commit

---
+++ KTP.java
@@ -0,0 +1,23 @@
+public class KTP {
+    public static void main(String[] entry) {
+        String NIK = "515172569852";
+        String nama = "Abisai Paskalis";
+        String tempat = "Sydney";
+        String tanggal_lahir = "25-12-2025";
+        String jenis_kelamin = "Laku";
+        String golongan = "-";
+        String alamat = "Ngawir";
+        String RT_RW = "005/014";
+        String Kel_Desa = "Ngawir Ngaten";
+        String Kecamatan = "Ngawir Ngulon";
+        String Agama = "Kepercayaan";
+        String Status = "Kawin";
+        String Pekerjaan = "Pelajar";
+        String Kewarganegaraan = "WNI";
+        String Berlaku = "SEUMUR HIDUP";
+        System.out.println("\tNIK\t = "+ NIK + "\nNama = "+ nama + "\nTempat/Tgl Lahir = " + tempat +","+ tanggal_lahir);
+        System.out.println("Jenis Kelamin\t = "+ jenis_kelamin + "\t\tGolongan Darah = "+ golongan + "\nAlamat = "+alamat);
+        System.out.println("\tRT/RW = "+RT_RW+ "\n\tKel/Desa = "+Kel_Desa+"\n\tKecamatan = "+Kecamatan+"\nAgama = "+Agama);
+        System.out.println("Status = "+Status+"\nPekerjaan = "+Pekerjaan+"\nKewarganegaraan = "+Kewarganegaraan+"\nBerlaku Hingga = "+Berlaku);
+    }
+}
---
+++ Latihan01.java
@@ -0,0 +1,8 @@
+public class Latihan01{
+	public static void main(String[] entry) {
+		int tahun = 2024;
+		String nim = "24410100061";
+		String nama = "Abisai Paskalis";
+		System.out.println("Mahasiswa dengan NIM = "+ nim + "\nyaitu " + nama + "\nmasuk Undika di tahun = "+tahun);
+	}
+}
